3658b57b9c2308a26535c5e239b65ddad90681be
Adrian van der Wal <a.vanderwaljnr@gmail.com>
2020-07-06 12:03:53 +0800

    0.1.17

--- data-updates.lua
+++ data-updates.lua
@@ -48,6 +48,10 @@ if mods['expanded-rocket-payloads'] then
   require('integrations/expanded-rocket-payloads')
 end
 
+if mods['Geothermal'] then
+  require('integrations/Geothermal')
+end
+
 if mods['Load-Furn'] then
   require('integrations/Load-Furn')
 end
